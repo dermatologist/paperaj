5ba482424dfd3b8933a63b6ca2e75aa58f8ac657
dermatologist <github@gulfdoctor.net>
2022-05-10 14:34:52 -0400

    copy all files to clean

--- article.sh
+++ article.sh
@@ -106,7 +106,7 @@ arxiv_latex_cleaner "$LATEXFOLDER" --verbose
 mv "${LATEXFOLDER}_arXiv" "$LATEXFOLDER/clean"
 cp -n -r "$LATEXFOLDER/paperaj/" "$LATEXFOLDER/clean/"
 cp -n -r "$LATEXFOLDER/media/" "$LATEXFOLDER/clean/"
-cp -n -r "$LATEXFOLDER/*.tex" "$LATEXFOLDER/clean/"
+cp -n -r "$LATEXFOLDER/*.*" "$LATEXFOLDER/clean/"
 
 
 echo "Creating ArXiv version"
